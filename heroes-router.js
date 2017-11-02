a3e7f69fdcf63342e7362e054c6892d0cc46d74c
lychtear <lychtear@gmail.com>
2017-11-02 11:50:42 +0200

    fix put post

--- heroes-router.js
+++ heroes-router.js
@@ -7,7 +7,10 @@ router.route('/')
         res.send(heroes);
     })
     .post((req, res) => {
-        heroes.find(hero => hero.id == req.body.id).name = req.body.name;
+        heroes.push({
+            id: req.body.id,
+            name: req.body.name,
+        });
         res.send(heroes);
     })
     .delete((req, res) => {
@@ -19,13 +22,17 @@ router.route('/:id')
     .get((req, res) => {
         res.send(heroes.filter(hero => hero.id == req.params.id));
     })
-    // Not clear ...
     .put((req, res) => {
-        const hero = {
-            id: req.body.id,
-            name: req.body.name,
+        const hero = heroes.find(hero => hero.id == req.body.id);
+        if (hero) {
+            hero.name = req.body.name
+        } else {
+            heroes.push({
+                id: req.body.id,
+                name: req.body.name,
+            });
         }
-        heroes.push(hero);
+        res.send(heroes);
     })
     .delete((req, res) => {
         heroes = heroes.filter(hero => hero.id != req.params.id);
